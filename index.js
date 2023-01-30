c339c229aefe2f767d06ce940c599ef2b8476369
oskarimattila <oskari.mattila@kotikone.fi>
2023-01-30 13:48:25 +0200

    3.19-22

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const { response } = require('express')
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
@@ -9,7 +8,7 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static('build'))
 
-morgan.token('body', function (req, res) {return JSON.stringify(req.body)})
+morgan.token('body', function (req) {return JSON.stringify(req.body)})
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 const errorHandler = (error, request, response, next) => {
@@ -17,6 +16,10 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   }
+  else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
   next(error)
 }
 
@@ -42,7 +45,7 @@ app.get('/api/persons/:id', (req, res, next) => {
   Person.findById(req.params.id)
     .then(person => {
       if (!person) {
-        return res.status(404).json({error: "person not found"})
+        return res.status(404).json({error: 'person not found'})
       }
       else res.json(person)
     })
@@ -52,7 +55,7 @@ app.get('/api/persons/:id', (req, res, next) => {
 app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndRemove(req.params.id)
     .then(person => {
-      if (!person) return res.status(404).json({error: "person not found"})
+      if (!person) return res.status(404).json({error: 'person not found'})
       else res.status(204).end()
     })
     .catch(error => next(error))
@@ -64,28 +67,24 @@ app.post('/api/persons', (req, res, next) => {
     name: body.name,
     number: body.number
   })
-  // This is redundant, might get deleted because this validation is done in the front end.
-  Person.find({})
-    .then(persons => {
-      if (persons.map(person => person.name.toLowerCase()).includes(body.name.toLowerCase())) {
-        return res.status(409).json({error: `${body.name} already exists`})
-      }
+  newPerson.save()
+    .then(savedPerson => {
+      res.json(savedPerson)
     })
     .catch(error => next(error))
-    newPerson.save()
-      .then(savedPerson => {
-        res.json(savedPerson)
-      })
-      .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
-  const body = req.body
+  const {name, number} = req.body
   const person = {
-    name: body.name,
-    number: body.number
+    name: name,
+    number: number
   }
-  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+  Person.findByIdAndUpdate(
+    req.params.id,
+    person,
+    {new: true, runValidators: true, context: 'query'}
+  )
     .then(updatedPerson => {
       res.json(updatedPerson)
     })
--- models/person.js
+++ models/person.js
@@ -1,21 +1,34 @@
 const mongoose = require('mongoose')
-
 mongoose.set('strictQuery', false)
 
 const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
+  .then(console.log('connected to MongoDB'))
   .catch((error) => {
     console.log('error connecting to MongoDB:', error.message)
   })
 
+const validateNumber = (number) => {
+  const validNumber = /\d{2,3}[-]\d{6,8}/
+  return validNumber.test(number)
+}
+
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String
+  name: {
+    type: String,
+    required: true,
+    minLength: 3
+    
+  },
+  number: {
+    type: String,
+    required: true,
+    validate: validateNumber,
+    minLength: 8,
+    maxLength: 12
+  }
 })
 
 personSchema.set('toJSON', {
